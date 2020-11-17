a09d52ea6fa03386f44444d02212bffe1f64bf1d
Piotr Kowańdy <phoennix@Phoennix-Mac.local>
2020-11-17 22:05:39 +0100

    reverse_string4 updated

--- src/reverse_string.cpp
+++ src/reverse_string.cpp
@@ -30,12 +30,14 @@ void switch_string3(std::string& word){
 }
 
 void switch_string4(std::string& word){
-	std::string::iterator it_begin = word.begin();
-	size_t size_of_word = word.size()-1;
+	if (word.size() > 1){
+		std::string::iterator it_begin = word.begin();
+		size_t size_of_word = word.size()-1;
 
-	while (size_of_word >= word.size()/2){
-		std::swap(*(it_begin), *(word.begin() + size_of_word));
-		size_of_word--;
-		it_begin++;
+		while (size_of_word >= word.size()/2){
+			std::swap(*(it_begin), *(word.begin() + size_of_word));
+			size_of_word--;
+			it_begin++;
+		}
 	}
 }
