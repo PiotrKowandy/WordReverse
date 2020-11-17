b5f166297f0e661ebdd57b70b7920a0d34fadd71
Piotr Kowańdy <phoennix@Phoennix-Mac.local>
2020-11-17 19:43:17 +0100

    new project

---
+++ src/reverse_string.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+
+void switch_string(std::string& word);
+void switch_string2(std::string& word);
+void switch_string3(std::string& word);
+void switch_string4(std::string& word);
